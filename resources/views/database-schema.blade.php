ada58ca487796ca0af649bcb0f486d3ea852fd21
Nikunj Kanetiya <kanetiya@gmail.com>
2017-07-09 14:47:34 +0530

    1. Printing styles fix. 2. Fix typo.

--- resources/views/database-schema.blade.php
+++ resources/views/database-schema.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Database List</div>
+                    <div class="panel-heading">Database Schema</div>
                     <div class="panel-body">
                         @foreach($tables as $table)
                             <table class="table table-bordered table-list">
