5010c4dc4cfe2184a8f9b58bc0b2f25ef925c1c5
Nikunj Kanetiya <kanetiya@gmail.com>
2017-07-11 13:36:31 +0530

    Converted schema view to vuejs inline template

--- resources/views/database-schema.blade.php
+++ resources/views/database-schema.blade.php
@@ -1,73 +1,84 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container" id="app">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <span class="list-inline">Database Schema : <strong>{{ $dbSchema->getName() }}</strong></span>
+                        <span class="list-inline">Database Schema : <strong>@{{ name }}</strong></span>
 
                         <button class="btn hidden-print" onclick="window.print()">Print</button>
                     </div>
                     <div class="panel-body">
-                        @foreach($dbSchema->getTables() as $table)
-                            <table class="table table-bordered table-list">
-                                <caption>{{ $table->name }}</caption>
-                                <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Type</th>
-                                        <th>Allow Null</th>
-                                        <th>Key/Attributes</th>
-                                        <th>Default</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($table->columns->toArray() as $column)
-                                        <tr>
-                                            <td>{{ $column->name }}</td>
-                                            <td>{{ $column->type }}</td>
-                                            <td>{{ $column->notnull ? '' : 'Nullable' }}</td>
-                                            <td>{{ $column->isPrimaryKey ? 'PK' : '' }}{{ $column->isForeignKey ? ' FK' : '' }}</td>
-                                            <td>{{ $column->default }} {{ $column->autoincrement ? 'Auto Increment' : '' }}</td>
-                                        </tr>
-                                    @endforeach
-                                    @if($table->foreignKeys)
-                                        <tr>
-                                            <td colspan="5">
-                                                <table width="100%" class="table table-bordered foreign-keys">
-                                                    <caption>Foreign Keys</caption>
-                                                        <thead>
-                                                            <tr>
-                                                                <th>Constraint Name</th>
-                                                                <th>Local Table</th>
-                                                                <th>Local Column</th>
-                                                                <th>Foreign Table</th>
-                                                                <th>Foreign Column</th>
-                                                            </tr>
-                                                        </thead>
-                                                    <tbody>
-                                                        @foreach($table->foreignKeys as $foreignKey)
-                                                            <tr>
-                                                                <td>{{ $foreignKey->getName() }}</td>
-                                                                <td>{{ $foreignKey->getLocalTableName() }}</td>
-                                                                <td>{{ implode(',', $foreignKey->getLocalColumns()) }}</td>
-                                                                <td>{{ $foreignKey->getForeignTableName() }}</td>
-                                                                <td>{{ implode(',', $foreignKey->getForeignColumns()) }}</td>
-                                                            </tr>
-                                                        @endforeach
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                    @endif
-                                </tbody>
-                            </table>
-                        @endforeach
+                        <table v-for="table in tables" v-if="tables" class="table table-bordered table-list">
+                            <caption>@{{ table.name }}</caption>
+                            <thead>
+                                <tr>
+                                    <th>Name</th>
+                                    <th>Type</th>
+                                    <th>Allow Null</th>
+                                    <th>Key/Attributes</th>
+                                    <th>Default</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr v-for="column in table.columns" v-if="table.columns">
+                                    <td>@{{ column.name }}</td>
+                                    <td>@{{ column.type }}</td>
+                                    <td>@{{ column.notnull ? '' : 'Nullable' }}</td>
+                                    <td>@{{ column.isPrimaryKey ? 'PK' : '' }} @{{ column.isForeignKey ? ' FK' : '' }}</td>
+                                    <td>@{{ column.autoincrement ? 'Auto Increment' : '' }}</td>
+                                </tr>
+                                <tr v-if="table.foreignKeys">
+                                    <td colspan="5">
+                                        <table width="100%" class="table table-bordered foreign-keys">
+                                            <caption>Foreign Keys</caption>
+                                            <thead>
+                                                <tr>
+                                                    <th>Constraint Name</th>
+                                                    <th>Local Table</th>
+                                                    <th>Local Column</th>
+                                                    <th>Foreign Table</th>
+                                                    <th>Foreign Column</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr v-for="foreignKey in table.foreignKeys">
+                                                    <td>@{{ foreignKey.constraint_name }}</td>
+                                                    <td>@{{ foreignKey.local_table }}</td>
+                                                    <td>@{{ foreignKey.local_columns | implode }}</td>
+                                                    <td>@{{ foreignKey.foreign_table }}</td>
+                                                    <td>@{{ foreignKey.foreign_columns | implode }}</td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('script')
+    <script>
+        Vue.filter('implode', function (value, piece, key) {
+            piece = piece ? piece : ', ';
+
+            if(_.isUndefined(key)) {
+                return value.join(piece);
+            }
+
+            return _.pluck(value, key).join(piece);
+        });
+
+        const app = new Vue({
+            el: '#app',
+            data: {!! json_encode($dbSchema->toArray()) !!}
+        });
+    </script>
 @endsection
