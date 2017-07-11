a75db6d4bab68926d103be03b799da17459287b0
Nikunj Kanetiya <kanetiya@gmail.com>
2017-07-11 13:17:50 +0530

    Modify controller as well as schema view as per ent data structure

--- app/Http/Controllers/SchemaController.php
+++ app/Http/Controllers/SchemaController.php
@@ -2,26 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use DB;
-use Config;
+use App\ENT\Database;
 
 class SchemaController extends Controller
 {
-    /**
-     * Default Connected DB
-     * @var string $currentDb
-     */
-    protected $currentDb;
-
-    /**
-     * SchemaController constructor.
-     * @param null $currentDb
-     */
-    public function __construct($currentDb = null)
-    {
-        $this->currentDb = DB::getDatabaseName();
-    }
-
     /**
      * Get basic welcome view with having link for all actions.
      *
@@ -41,7 +25,7 @@ class SchemaController extends Controller
         $databases = \DB::getDoctrineSchemaManager()->listDatabases();
 
         return view('database', compact('databases'))
-            ->with('currentDb', $this->currentDb);
+            ->with('currentDb', \DB::getDatabaseName());
     }
 
     /**
@@ -52,68 +36,8 @@ class SchemaController extends Controller
      */
     public function getDatabaseSchemaView($dbName = null)
     {
-        if(!$dbName) $dbName = $this->currentDb;
+        $dbSchema = new Database($dbName);
 
-        // Remaining fixing name from slug
-        $tables = $this->getTables($dbName);
-
-        return view('database-schema', compact('tables', 'dbName'));
-    }
-
-    /**
-     * Return the list of tables details for the database connection.
-     *
-     * @param $dbName
-     * @return \Doctrine\DBAL\Schema\Table[]
-     */
-    protected function getTables($dbName)
-    {
-        if($this->currentDb != $dbName) {
-            $this->connectToDatabase($dbName);
-        }
-
-        return DB::getDoctrineSchemaManager()->listTables();
-    }
-
-    /**
-     * Change mysql connection and connect to other database
-     *
-     * @param string $dbName
-     */
-    protected function connectToDatabase($dbName)
-    {
-        $defaultConfig = Config::get($this->getDBConfigKey());
-
-        // Set our targeted database name
-        $defaultConfig['database'] = $dbName;
-
-        // Our new config
-        Config::set($this->getDBConfigKey('docer'), $defaultConfig);
-
-        // Set our newly created config as default connection
-        DB::setDefaultConnection('docer');
-        $this->registerEnums();
-    }
-
-    /**
-     * Get database connection config key
-     * @param null $connectName
-     * @return string
-     */
-    protected function getDBConfigKey($connectName = null)
-    {
-        if(!$connectName) $connectName = DB::getDefaultConnection();
-
-        return 'database.connections.' . $connectName;
-    }
-
-    /**
-     * Register enum types as string.
-     */
-    protected function registerEnums()
-    {
-        $platform = DB::getDoctrineSchemaManager()->getDatabasePlatform();
-        $platform->registerDoctrineTypeMapping('enum', 'string');
-        $platform->registerDoctrineTypeMapping('json', 'text');
+        return view('database-schema', compact('dbSchema'));
     }
 }
--- resources/views/database-schema.blade.php
+++ resources/views/database-schema.blade.php
@@ -6,14 +6,14 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <span class="list-inline">Database Schema : <strong>{{ $dbName }}</strong></span>
+                        <span class="list-inline">Database Schema : <strong>{{ $dbSchema->getName() }}</strong></span>
 
                         <button class="btn hidden-print" onclick="window.print()">Print</button>
                     </div>
                     <div class="panel-body">
-                        @foreach($tables as $table)
+                        @foreach($dbSchema->getTables() as $table)
                             <table class="table table-bordered table-list">
-                                <caption>{{ $table->getName() }}</caption>
+                                <caption>{{ $table->name }}</caption>
                                 <thead>
                                     <tr>
                                         <th>Name</th>
@@ -24,41 +24,40 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <?php $primaryKeys = $table->getPrimaryKey();
-                                          $primaryKeys = $primaryKeys ? $primaryKeys->getColumns() :[]
-                                    ?>
-                                    @foreach($table->getColumns() as $column)
+                                    @foreach($table->columns->toArray() as $column)
                                         <tr>
-                                            <td>{{ $column->getName() }}</td>
-                                            <td>{{ $column->getType()->getName() . ($column->getLength() ? "({$column->getLength()})" :'') }}</td>
-                                            <td>{{ $column->getNotnull() ? null : 'Nullable' }}</td>
-                                            <td>{{ $primaryKeys && in_array($column->getName(), $primaryKeys) ? 'Primary' : '' }}</td>
-                                            <td>{{ $column->getDefault() }}</td>
+                                            <td>{{ $column->name }}</td>
+                                            <td>{{ $column->type }}</td>
+                                            <td>{{ $column->notnull ? '' : 'Nullable' }}</td>
+                                            <td>{{ $column->isPrimaryKey ? 'PK' : '' }}{{ $column->isForeignKey ? ' FK' : '' }}</td>
+                                            <td>{{ $column->default }} {{ $column->autoincrement ? 'Auto Increment' : '' }}</td>
                                         </tr>
                                     @endforeach
-                                    <?php $foreignKeys = $table->getForeignKeys();
-                                    ?>
-                                    @if($foreignKeys)
+                                    @if($table->foreignKeys)
                                         <tr>
                                             <td colspan="5">
                                                 <table width="100%" class="table table-bordered foreign-keys">
                                                     <caption>Foreign Keys</caption>
-                                                    <thead>
-                                                        <th>Constraint Name</th>
-                                                        <th>Local Table</th>
-                                                        <th>Local Column</th>
-                                                        <th>Foreign Table</th>
-                                                        <th>Foreign Column</th>
-                                                    </thead>
-                                                    @foreach($foreignKeys as $foreignKey)
-                                                        <tr>
-                                                            <td>{{ $foreignKey->getName() }}</td>
-                                                            <td>{{ $foreignKey->getLocalTableName() }}</td>
-                                                            <td>{{ implode(',', $foreignKey->getLocalColumns()) }}</td>
-                                                            <td>{{ $foreignKey->getForeignTableName() }}</td>
-                                                            <td>{{ implode(',', $foreignKey->getForeignColumns()) }}</td>
-                                                        </tr>
-                                                    @endforeach
+                                                        <thead>
+                                                            <tr>
+                                                                <th>Constraint Name</th>
+                                                                <th>Local Table</th>
+                                                                <th>Local Column</th>
+                                                                <th>Foreign Table</th>
+                                                                <th>Foreign Column</th>
+                                                            </tr>
+                                                        </thead>
+                                                    <tbody>
+                                                        @foreach($table->foreignKeys as $foreignKey)
+                                                            <tr>
+                                                                <td>{{ $foreignKey->getName() }}</td>
+                                                                <td>{{ $foreignKey->getLocalTableName() }}</td>
+                                                                <td>{{ implode(',', $foreignKey->getLocalColumns()) }}</td>
+                                                                <td>{{ $foreignKey->getForeignTableName() }}</td>
+                                                                <td>{{ implode(',', $foreignKey->getForeignColumns()) }}</td>
+                                                            </tr>
+                                                        @endforeach
+                                                    </tbody>
                                                 </table>
                                             </td>
                                         </tr>
